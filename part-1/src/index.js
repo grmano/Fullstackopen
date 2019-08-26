b4dd5a0901302a802f8795886fb87d45021f560a
Lucas Germano <germano@poli.ufrj.br>
2019-08-25 23:15:19 -0300

    ex 1.1: refactored Total

--- part-1/src/index.js
+++ part-1/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 
 import Header from './components/Header'
 import Content from './components/Content'
+import Total from './components/Total'
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -16,12 +17,12 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content 
+      <Content
         part1={part1} exercises1={exercises1}
         part2={part2} exercises2={exercises2}
         part3={part3} exercises3={exercises3}
       />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total sum={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
